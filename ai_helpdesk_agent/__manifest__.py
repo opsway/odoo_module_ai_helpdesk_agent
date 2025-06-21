d1abdf54c23c526ff22b48c2766017ad0667f107
Ros <sinvigil@gmail.com>
2025-06-21 20:31:19 +0200

    [FIX] migrate from 16 to 17 odoo version

--- ai_helpdesk_agent/__manifest__.py
+++ ai_helpdesk_agent/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'AI Helpdesk Agent',
-    'version': '16.0.0.1.2',
+    'version': '17.0.0.1.0',
     'summary': 'Process tickets with AI and send responses to customers.',
     'description': 'AI for helpdesk tickets',
     'category': 'Services/Helpdesk',
